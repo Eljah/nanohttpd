5da50fdfb362e348a593acd2ecd857d0fb012a98
Ilya <eljah@mail.ru>
2015-08-21 00:18:58 +0300

    back to emty text responces

--- samples/src/main/java/fi/iki/elonen/CrawlerTest1.java
+++ samples/src/main/java/fi/iki/elonen/CrawlerTest1.java
@@ -39,7 +39,7 @@ public class CrawlerTest1 extends NanoHTTPD {
                     //fis.close();     //todo comment it; doesn't respond http correctly with this line
                     //Response resp = new Response(Response.Status.NOT_FOUND, MIME_HTML, "Not fount");
                     Response resp = new Response(Response.Status.OK, MIME_HTML, "Not fount");
-                    resp.addHeader("Location","http://img06.blogcu.com/v2/images/orj/b/i/r/birturkbilgesi/birturkbilgesi_1396779218152.jpg");
+                    //resp.addHeader("Location","http://img06.blogcu.com/v2/images/orj/b/i/r/birturkbilgesi/birturkbilgesi_1396779218152.jpg");
                     return resp;
                 } catch (FileNotFoundException e) {
                     // TODO Auto-generated catch block
