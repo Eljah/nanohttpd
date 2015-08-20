5c8291a205e745f613d14183da6aa12693ae6ef1
Ilya <eljah@mail.ru>
2015-08-20 23:55:58 +0300

    back to emty text responces

--- samples/src/main/java/fi/iki/elonen/CrawlerTest1.java
+++ samples/src/main/java/fi/iki/elonen/CrawlerTest1.java
@@ -38,7 +38,7 @@ public class CrawlerTest1 extends NanoHTTPD {
                     System.out.println("Requesting /favicon.ico");
                     //fis.close();     //todo comment it; doesn't respond http correctly with this line
                     //Response resp = new Response(Response.Status.NOT_FOUND, MIME_HTML, "Not fount");
-                    Response resp = new Response(Response.Status.REDIRECT, MIME_HTML, "Not fount");
+                    Response resp = new Response(Response.Status.OK, MIME_HTML, "Not fount");
                     resp.addHeader("Location","http://img06.blogcu.com/v2/images/orj/b/i/r/birturkbilgesi/birturkbilgesi_1396779218152.jpg");
                     return resp;
                 } catch (FileNotFoundException e) {
@@ -87,6 +87,7 @@ public class CrawlerTest1 extends NanoHTTPD {
             }
             String msg = "<!DOCTYPE html>\n" +
                     "<html lang=\"en\">\n" +
+                    "<!--<link rel=\"icon\" type=\"image/png\" href=\"/favicon3.ico\" />-->\n" +
                     //"<!--<link rel=\"icon\" type=\"image/png\" href=\"/favicon2.ico\" />-->\n" +
                     //http://img06.blogcu.com/v2/images/orj/b/i/r/birturkbilgesi/birturkbilgesi_1396779218152.jpg
                     //"<link rel=\"icon\" type=\"image/png\" href=\"/http://img06.blogcu.com/v2/images/orj/b/i/r/birturkbilgesi/birturkbilgesi_1396779218152.jpg\" />\n"  +
